b367e32d6d3b53c8f9a79aa5209e14eae3958ffa
Cybertron-ap <109458949+aparnab-02@users.noreply.github.com>
2023-01-19 21:34:01 +0530

    first_commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import "./App.css"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Login from "./Login"
+
+function App() {
+  return (
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/login" element={<Login />} />
+    //   </Routes>
+    // </BrowserRouter>
+    <Login/>
+    
+  )
+}
+
+export default App
